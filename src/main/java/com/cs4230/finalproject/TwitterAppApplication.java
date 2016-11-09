5b0612eefd73f12d022770f20f4135f6617cee69
Andrew Lane <andrew.bryan12@gmail.com>
2016-11-08 23:35:43 -0700

    initial commit - Spring MVC starting point

---
+++ src/main/java/com/cs4230/finalproject/TwitterAppApplication.java
@@ -0,0 +1,12 @@
+package com.cs4230.finalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterAppApplication.class, args);
+	}
+}
