74467bd0e4453d8d5752bf116abb70c226eb7777
Andrew Lane <andrew.bryan12@gmail.com>
2016-11-09 21:04:53 -0700

    adding placeholder twitter implementation

--- src/main/java/com/cs4230/finalproject/Application.java
+++ src/main/java/com/cs4230/finalproject/Application.java
@@ -3,9 +3,10 @@ package com.cs4230.finalproject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
-public class WebApplication {
+public class Application {
     public static void main(String[] args) throws Exception {
-        SpringApplication.run(WebApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
