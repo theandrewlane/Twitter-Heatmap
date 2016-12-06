497c0a7ba600cabf7e0c3c57ceadab5ef0efdc76
thomann061 <thomann061@gmail.com>
2016-12-05 20:23:03 -0700

    Filter and Geocode tweet and send coordinates to map

--- src/main/java/com/cs4230/finalproject/controller/TwitterStreamService.java
+++ src/main/java/com/cs4230/finalproject/controller/TwitterStreamService.java
@@ -1,6 +1,10 @@
-package com.cs4230.finalproject.model;
+package com.cs4230.finalproject.controller;
 
-import com.cs4230.finalproject.controller.TweetAnalysis;
+import com.cs4230.finalproject.Utilities.KeywordReader;
+import com.cs4230.finalproject.model.TweetAnalysis;
+import com.cs4230.finalproject.model.TweetFilter;
+import com.cs4230.finalproject.model.TweetGeocode;
+import com.google.gson.JsonObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -10,6 +14,8 @@ import org.springframework.ui.Model;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.apache.coyote.http11.Constants.a;
 
 /**
  * Created by andrewlane on 11/16/16.
@@ -21,7 +27,16 @@ public class TwitterStreamService {
     private SimpMessagingTemplate template;
     private int tweetCount = 0;
 
-    private TweetAnalysis ta = new TweetAnalysis();
+    private TweetAnalysis ta;
+
+    @Autowired
+    private KeywordReader keywordReader;
+
+    @Autowired
+    private TweetFilter tf;
+
+    @Autowired
+    private TweetGeocode tg;
 
     @Autowired
     public TwitterStreamService(SimpMessagingTemplate template) {
@@ -29,16 +44,20 @@ public class TwitterStreamService {
         this.template = template;
     }
 
+    public List<String> getKeywordList() {
+        keywordReader.init("src/main/files/party-keywords.txt", new ArrayList<String>());
+        keywordReader.readInKeywords();
+        return keywordReader.getKeywordList();
+    }
 //    @MessageMapping("/tweetLocation")
 //    public void setLocation(String location) {
 ////Data from view should be sent here
 //
 //    }
 
-
     @SendTo("/tweets/stream")
-    public void tweetStream(Tweet tweet) {
-        this.template.convertAndSend("/tweets/stream", tweet);
+    public void tweetStream(JsonObject obj) {
+        this.template.convertAndSend("/tweets/stream", obj.toString());
     }
 
 //    private final org.slf4j.Logger log = LoggerFactory.getLogger(TwitterStreamService.class);
@@ -50,6 +69,7 @@ public class TwitterStreamService {
     public List<Tweet> streamApi(Model model) throws InterruptedException {
         List<StreamListener> listeners = new ArrayList<>();
         StreamListener streamListener = new StreamListener() {
+
             @Override
             public void onWarning(StreamWarningEvent warningEvent) {
                 // TODO Auto-generated method stub
@@ -58,13 +78,20 @@ public class TwitterStreamService {
 
             @Override
             public void onTweet(Tweet tweet) {
-                //add to analysis controller
-                ta.add(tweet);
-                //ta.print();
-                tweets.add(tweet);
-                tweetStream(tweet);
-                tweetCount++;
-                model.addAttribute("tweetCount", tweetCount);
+                //Filter each tweet
+                Tweet filteredTweet = tf.filter(getKeywordList(), tweet);
+                //Geocode the filtered tweet
+                JsonObject coordinates = tg.geocode(filteredTweet);
+                //Analyze each tweet
+                //ta.add(filteredTweet);
+                // TODO
+                //Send to GUI
+                if(coordinates != null) {
+                    tweets.add(filteredTweet);
+                    tweetStream(coordinates);
+                    tweetCount++;
+                    model.addAttribute("tweetCount", tweetCount);
+                }
             }
 
             @Override
@@ -82,10 +109,10 @@ public class TwitterStreamService {
 
         listeners.add(streamListener);
         //This sets the GeoCode (-122.75,36.8,-121.75,37.8) of San Francisco(South-West and North-East) region as given in below twitter docs
-        Float west = -124.84f;
-        Float south = 24.39f;
-        Float east = -66.88f;
-        Float north = 49.38f;
+        Float west = -111.993474f;
+        Float south = 33.332718f;
+        Float east = -111.825695f;
+        Float north = 33.4872f;
 
         FilterStreamParameters filterStreamParameters = new FilterStreamParameters();
         filterStreamParameters.addLocation(west, south, east, north);
