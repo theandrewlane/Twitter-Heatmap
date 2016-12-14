c8cc2063e93343ef7b4010091933e4b32905e631
thomann061 <thomann061@gmail.com>
2016-12-14 15:55:05 -0700

    Add custom tweet user

---
+++ src/main/java/com/cs4230/finalproject/model/TweetUser.java
@@ -0,0 +1,37 @@
+package com.cs4230.finalproject.model;
+
+import org.springframework.social.twitter.api.Tweet;
+
+/**
+ * Created by jthomann on 12/14/16.
+ */
+public class TweetUser {
+    private String lat;
+    private String lng;
+    private String name;
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
